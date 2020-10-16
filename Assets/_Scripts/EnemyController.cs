f832ae4cb6e1e2358d5ba3953f4759f9b989e17b
FlavioJorge AraujoSilva <silvaflavio267@gmail.com>
2020-10-16 11:48:27 -0400

    Lab Completed

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float horizontalSpeed;
+    [SerializeField] float horizontalBoundary;
+
+    float direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _CheckBounds()
+    {
+        /// check if on the right boundry
+        if (transform.position.x >= horizontalBoundary)
+        {
+            direction = -1.0f;
+        }
+
+        /// check if on the right boundry
+        else if (transform.position.x <= -horizontalBoundary)
+        {
+            direction = 1.0f;
+        }
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    
+}
